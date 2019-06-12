b14fa5271270b9910324d8813f6247853fa2126a
LuisValgoi <luisvalgoi@outlook.com>
2019-06-12 01:10:10 -0300

    decoupled routes from index to separated route file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,18 @@
+// require
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
+// init application
 const app = express();
 
+// database connection
 mongoose.connect('mongodb+srv://SEMANA:Initial1@cluster0-q7f0j.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
-})
-
-app.get('/', (req, res) => {
-    return res.send(`Hello ${req.query.name}`);
 });
 
+// custom layer / files
+app.use(routes);
+
+// attach server to the port
 app.listen(3333);
