c97036cd558517dfbd218d7e4208e55ef180a1ba
LuisValgoi <luisvalgoi@outlook.com>
2019-06-12 01:00:11 -0300

    added connection to the mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://SEMANA:Initial1@cluster0-q7f0j.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+})
 
 app.get('/', (req, res) => {
     return res.send(`Hello ${req.query.name}`);
