8e9ab9904ae9a13e37fc2490d6259307387b73dc
LuisValgoi <luisvalgoi@outlook.com>
2019-06-12 01:34:29 -0300

    added first version of the post model and controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const PostController = require('./controllers/PostController');
 
 const routes = new express.Router();
 
-routes.get('/', (req, res) => {
+routes.get('/user', (req, res) => {
     return res.send(`Hello ${req.query.name}`);
 });
+
+routes.post('/posts', PostController.store);
 
 module.exports = routes;
