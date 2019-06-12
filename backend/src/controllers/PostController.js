d3eb05d458f1a030eccc3882ef2ef7988188d0b8
LuisValgoi <luisvalgoi@outlook.com>
2019-06-12 01:42:03 -0300

    updated post http call to print postjson infos

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -6,6 +6,11 @@ module.exports = {
     },
 
     async store(req, res) {
+        console.log('============ BODY');
+        console.log(req.body);
+        console.log('============ FILE');
+        console.log(req.file);
+        
         return res.json({
             ok: true
         });
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const multer = require('multer');
 const PostController = require('./controllers/PostController');
 
 const routes = new express.Router();
+const upload = multer();
 
 routes.get('/user', (req, res) => {
     return res.send(`Hello ${req.query.name}`);
 });
 
-routes.post('/posts', PostController.store);
+routes.post('/posts', upload.single('image'), PostController.store);
 
 module.exports = routes;
