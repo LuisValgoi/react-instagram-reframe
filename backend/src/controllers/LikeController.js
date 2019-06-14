cbe113675f530eddbfc8a2df89c9306977aa08d7
LuisValgoi <luisvalgoi@outlook.com>
2019-06-13 22:26:11 -0300

    added function which increments likes

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -2,6 +2,18 @@ const Post = require('../models/Post');
 
 module.exports = {
     async store(req, res) {
-        return res.json({ok: true});
+        // get the image ID by URL
+        const id = req.params.id;
+        
+        // get the post according to the give id
+        const post = await Post.findById(id);
+
+        // incremented its likes
+        post.likes += 1;
+
+        // saved the object
+        await post.save();
+
+        return res.json(post);
     }
 };
