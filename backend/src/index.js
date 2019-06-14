3d92e31a122679f6f38f60c749d251db8f4d3e5b
LuisValgoi <luisvalgoi@outlook.com>
2019-06-13 22:55:29 -0300

    added use which exposes the files and its content through URI

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const path = require('path');
 
 // init application
 const app = express();
@@ -10,6 +11,10 @@ const app = express();
 mongoose.connect('mongodb+srv://SEMANA:Initial1@cluster0-q7f0j.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
+
+// exposes the files and its content through URI so the frontend can grab it
+const filesPath = path.resolve(__dirname, '..', 'uploads', 'resized');
+app.use('/files', express.static(filesPath));
 
 // custom layer / files
 app.use(routes);
