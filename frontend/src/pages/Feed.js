13d0740f6b0f24d81484e9e67930f08a5048def4
LuisValgoi <luisvalgoi@outlook.com>
2019-06-20 01:44:20 -0300

    added initial version of new and feed custom components

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// style class written component
+class Feed extends Component {
+    render() {
+        return (
+            <h1>Feed</h1>
+        );
+    }
+}
+
+export default Feed;
